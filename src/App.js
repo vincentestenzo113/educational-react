cf39bca15c88ce07a540d6342bb7cf050420564e
Gmarc27 <collados.gmarc06@gmail.com>
2024-03-14 16:34:51 +0800

    fix routing -v

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './components/Login';
 import Signup from './components/SignUp';
 import Dashboard from './components/Dashboard';
+import CourseContent from './components/CourseContent';
+import Quizzes from './components/Quizzes';
+import DiscussionForum from './components/DiscussionForums';
+
 
 const App = () => {
     return (
@@ -10,9 +14,11 @@ const App = () => {
             <div>
                 <Routes>
                     <Route path="/" element={<Login />} />
-                    <Route path="/login" element={<Login />} />
                     <Route path="/signup" element={<Signup />} />
                     <Route path="/dashboard" element={<Dashboard />} />
+                    <Route path="/course-content" element={<CourseContent />} />
+                    <Route path="/quizzes" element={<Quizzes />} />
+                    <Route path="/discussion-forums" element={<DiscussionForum />} />
                 </Routes>
             </div>
         </Router>
